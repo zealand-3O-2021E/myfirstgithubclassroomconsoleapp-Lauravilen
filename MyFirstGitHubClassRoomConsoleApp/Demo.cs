b4e6945b64f1ec627900532d7e68ed1549e32888
Lauravilen <44665204+Lauravilen@users.noreply.github.com>
2021-08-31 14:38:32 +0200

    Added "Demo" class. Ready to take over the world.

---
+++ MyFirstGitHubClassRoomConsoleApp/Demo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp
+{
+    class Demo
+    {
+        public string _commander { get; set; }
+        public int _numberOfTroops { get; set; }
+        public string _warCry { get; set; }
+
+        public Demo(string commander, int numberOfTroops, string warCry)
+        {
+            _commander = commander;
+            _numberOfTroops = numberOfTroops;
+            _warCry = warCry;
+        }
+
+        public string ArmyPresent()
+        {
+            string presentArmy = $"Commander {_commander} presents an army of {_numberOfTroops}. {_warCry}";
+            return presentArmy;
+
+        }
+    }
+}
